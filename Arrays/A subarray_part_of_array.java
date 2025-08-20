59e149e97c0ba800b7031bdb9ba8030c7798ac23
Manisha Samanta <manisha7.work@gmail.com>
2025-08-20 17:50:22 +0530

    subarray
    A subarray is a continuous part of an array.

---
+++ Arrays/A subarray_part_of_array.java
@@ -0,0 +1,15 @@
+class Solution {
+    int maxSubarraySum(int[] arr) {
+        // Code here
+        
+        int maxSum= arr[0];
+        int currentSum=arr[0];
+        for(int i=1; i< arr.length; i++)
+        {
+            currentSum=Math.max(arr[i],currentSum +arr[i]);
+            maxSum =Math.max(maxSum,currentSum);
+        }
+        return maxSum;
+       
+    }
+}
